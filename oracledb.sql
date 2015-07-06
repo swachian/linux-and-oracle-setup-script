99015ba127396fb56450472ab4e096736c8c0548
swachian <zhangyu34@gmail.com>
2015-07-06 13:38:59 +0800

    Update oracledb.sql

--- oracledb.sql
+++ oracledb.sql
@@ -34,3 +34,15 @@ alter tablespace data2 add datafile  '/u02/oradata/aaa/data2_06.dbf' size 20000m
 
 
 nohup imp userid=aaa_v2/aaa file=aaa.dmp fromuser=aaa_v2 touser=aaa_v2 buffer=10000000 log=./imp.log &
+
+
+select 
+b.file_name 物理文件名,
+b.tablespace_name 表空间,
+b.bytes/1024/1024 大小M,
+(b.bytes-sum(nvl(a.bytes,0)))/1024/1024  已使用M,
+substr((b.bytes-sum(nvl(a.bytes,0)))/(b.bytes)*100,1,5)  利用率 
+from dba_free_space a,dba_data_files b 
+where a.file_id=b.file_id 
+group by b.tablespace_name,b.file_name,b.bytes 
+order by b.tablespace_name
